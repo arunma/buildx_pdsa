80b6efef41b69251315124453451814f1268e87d
Arun Manivannan <arun@arunma.com>
2023-02-25 01:24:17 +0800

    Attemping a 0.1 release

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,7 @@
+//! Prolly is a collection of probabilistic data structures implemented in Rust.
+//! These data structures are useful for various applications where approximate
+//! answers or approximations to subsets are sufficient.
+//!
+
 pub mod error;
 pub mod membership;
