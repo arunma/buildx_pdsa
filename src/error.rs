cca97ebdf214ea84e9ced12068d370c79000673a
Arun Manivannan <arun@arunma.com>
2023-03-19 08:45:49 +0800

    Extracting out CBF code for blog

--- src/error.rs
+++ src/error.rs
@@ -5,7 +5,7 @@ pub type PDSAResult<T> = std::result::Result<T, PDSAError>;
 #[derive(Debug, Error, PartialEq)]
 pub enum PDSAError {
     #[error("Input value error: {0:?}")]
-    Input(String),
+    InputError(String),
     #[error("Internal error: {0:?}")]
     Internal(String),
 }
